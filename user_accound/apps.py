7bfba89b6276b787e4ce9dcafb5fa3f306b040ab
RISWAN-33 <mhdriswan706@gmail.com>
2026-03-16 14:02:00 +0530

    first commit

---
+++ user_accound/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+
+
+class UserAccoundConfig(AppConfig):
+    name = 'user_accound'
---
+++ user_accound/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+class CustomUser(AbstractUser):
+
+    bio = models.TextField(blank=True)
+
+    profile_picture = models.ImageField(
+        upload_to='profile_picture/',
+        blank=True
+    )
+    dob = models.DateField(null=True,blank=True)
+
+    is_private = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.username
+    # str is used for readability
+
+
+    def followers_count(self):
+        return self.followers.count()
+    def following_count(self):
+        return self.following.count()
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        indexes =[
+            models.Index(fields=["username"]),
+            models.Index(fields=["email"]),
+        ]
+
+
+
+
+class follow(models.Model):
+
+    follower = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+
+    following = models.ForeignKey(
+        CustomUser,
+            on_delete=models.CASCADE,
+        related_name="followers"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints =[
+            models.UniqueConstraint(
+                fields=["follower","following"],
+                name="Unique_follow_relationship"
+            )
+        ]
+        indexes =[
+            models.Index(fields=["follower"]),
+            models.Index(fields=["following"]),
+        ]
+
+
+
+    def clean(self):
+        if self.follower == self.following:
+            raise ValidationError("User cannot follow themselves.")
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.follower.username} → {self.following.username}"
